ec6767a6a84dca6dba605d05b0814cb83ec3e5b6
Toni Kini Foster <tdotstorm@gmail.com>
2022-04-28 18:47:51 -0400

    menu toggler works now!

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,7 @@
 function getUsername() {
     var x = document.getElementById("username").value;
 }
+
+function showUserMenu() {
+    document.getElementById("user-menu").style.visibility = "hidden";
+}
